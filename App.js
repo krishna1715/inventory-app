69514a69f177e776c548441e54b4004851ff242a
krishna1715 <krishna.jagnani@gmail.com>
2025-04-04 19:14:22 +0530

    Added public folder for assets
    Added public folder for assets

---
+++ App.js
@@ -0,0 +1,14 @@
+
+// App.js
+import React from "react";
+import LoginRegister from "./LoginRegister";
+
+function App() {
+  return (
+    <div>
+      <LoginRegister />
+    </div>
+  );
+}
+
+export default App;
